62ab4ea0604d358037db1570511f7989486426bb
techhouse_150908 <kmaehara@mel.co.jp>
2024-01-24 15:03:42 +0900

    再起動を繰り返す現象が止まった。

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,31 @@
 #include <Arduino.h>
 //#include <lvgl.h>
 #include <TFT_eSPI.h> // ILI9488ドライバを含むライブラリ
-#include "common.h"
 //#include <WiFi.h> // WiFiライブラリをインクルード
 
 TFT_eSPI tft = TFT_eSPI();  // TFTインスタンスを作成
 
 int currentScreen = 0;  // 現在のスクリーン番号
 
+void drawScreen(int screenNumber);
+void drawButton(String title, int x, int y);
+void touchEventTask(void *parameter);
 
 void setup() {
+    Serial.begin(115200);
+    Serial.println("setup start");
     //start_setup();
     tft.init();  // TFTの初期化
     tft.setRotation(1);  // 画面の向きを設定
+
     drawScreen(currentScreen);  // 最初のスクリーンを描画
 
-    //WiFi.begin(ssid, password);
+    xTaskCreate(touchEventTask, "TouchTask", 10000, NULL, 1, NULL);
+
+
+    Serial.println("setup done");
 }
+
 
 
 
@@ -62,4 +71,38 @@ void previousScreen() {
   if (currentScreen == 0) currentScreen = 3;
   else currentScreen--;
   drawScreen(currentScreen);  // 画面を描画
+}
+
+
+
+void touchEventTask(void *parameter) {
+    Serial.println("touchEventTask start");
+    uint16_t x, y; // タッチ座標を保存する変数
+    int testCount = 0;
+
+    while(true) {
+
+        #if 1
+
+        if (tft.getTouch(&x, &y)) {
+            // 「Next」ボタンの範囲内かどうかをチェック
+
+            #if 0
+            if (x >= 120 && x <= (120 + 100) && y >= 200 && y <= (200 + 40)) {
+                nextScreen();
+            }
+            // 「Back」ボタンの範囲内かどうかをチェック
+            else if (x >= 0 && x <= 100 && y >= 200 && y <= (200 + 40)) {
+                previousScreen();
+            }
+            #endif
+        }
+        #endif
+
+        //Serial.println("touchEventTask " + String(testCount++));
+        delay(100); // CPU使用率を下げるためにわずかな遅延を挿入
+
+
+    }
+    vTaskDelete(NULL); // タスクを削除（この例では到達しませんが、形式として）
 }
