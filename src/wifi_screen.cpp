2acc04d1a76188b7b9b97ffda1c8f152cf988a49
techhouse_150908 <kmaehara@mel.co.jp>
2024-01-26 11:47:37 +0900

    SSID表示やIPアドレス表示等のプログラムを改造

--- src/wifi_screen.cpp
+++ src/wifi_screen.cpp
@@ -27,6 +27,19 @@ void task_connectToWiFi(void * parameter) {
 }
 
 
+// WiFi接続のステータスを確認し、接続が完了している場合にSSIDとIPアドレスを表示する関数
+void displayWiFiInfo(lv_obj_t *label_ssid, lv_obj_t *label_ip) {
+    if (WiFi.status() == WL_CONNECTED) {
+        lv_label_set_text(label_ssid, WiFi.SSID().c_str());
+        lv_label_set_text(label_ip, WiFi.localIP().toString().c_str());
+    } else {
+        lv_label_set_text(label_ssid, "SSID: Not connected");
+        lv_label_set_text(label_ip, "IP: Not connected");
+    }
+}
+
+
+
 void create_wifi_screen(lv_obj_t *scr) {
 
     Serial.println("create_wifi_screen start");
@@ -35,13 +48,14 @@ void create_wifi_screen(lv_obj_t *scr) {
     lv_obj_t* label_ssid = lv_label_create(scr);
     lv_obj_t* label_ip = lv_label_create(scr);
 
+    // ラベルの位置を設定（必要に応じて調整してください）
+    lv_obj_align(label_ssid, LV_ALIGN_CENTER, 0, -30);
+    lv_obj_align(label_ip, LV_ALIGN_CENTER, 0, 30);
 
-    if (WiFi.status() == WL_CONNECTED) {
-        lv_label_set_text(label_ssid, WiFi.SSID().c_str());
-        lv_label_set_text(label_ip, WiFi.localIP().toString().c_str());
-    }
+    // WiFi情報を表示
+    displayWiFiInfo(label_ssid, label_ip);
 
-    //add_navigation_buttons(screen5, screen1, screen4);
+    add_navigation_buttons(screen5, screen1, screen4);
 
     Serial.println("create_wifi_screen End");
 }
