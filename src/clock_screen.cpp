d8d95f86b77de497b8509e95d1dd9b4d95c9b69e
techhouse_150908 <kmaehara@mel.co.jp>
2024-01-26 15:46:13 +0900

    表示時刻をNTPサーバから取得した現在時刻に更新OK

--- src/clock_screen.cpp
+++ src/clock_screen.cpp
@@ -114,11 +114,44 @@ void lvgl_screen_setup() {
 LV_FONT_DECLARE(jpFont04);
 
 
+// グローバル変数としてタイマーカウンタを宣言
+//unsigned long lastNtpUpdateTime = 0;
+const unsigned long ntpUpdateInterval = 86400000; // 24時間 = 86400000ミリ秒
+unsigned long lastNtpUpdateTime = ULONG_MAX - ntpUpdateInterval; // 初回実行時に必ずNTP更新が行われるように設定
+
 
 // グローバル変数として時刻を表示するラベルを宣言
 lv_obj_t *time_label;
 
 void update_time(lv_timer_t *timer) {
+
+    // Wi-Fi接続状態を確認
+    if(WiFi.status() == WL_CONNECTED) {
+        // 現在の時間をミリ秒単位で取得
+        unsigned long currentMillis = millis();
+
+        // 前回のNTP更新から24時間が経過していれば更新
+        if(currentMillis - lastNtpUpdateTime >= ntpUpdateInterval) 
+        {
+            timeClient.update(); // NTPクライアントを更新
+            Serial.println("NTP Update!");
+            Serial.println("NTPサーバーから時刻を更新しました。");
+
+            // NTPサーバーから取得した時刻を表示
+            Serial.print("現在時刻: ");
+            Serial.print(timeClient.getHours());
+            Serial.print(":");
+            Serial.print(timeClient.getMinutes());
+            Serial.print(":");
+            Serial.println(timeClient.getSeconds());
+
+            // GMTオフセットとサマータイムの設定（例：日本標準時の場合）
+            configTime(3600 * 9, 0, "pool.ntp.org", "time.nist.gov");
+
+            lastNtpUpdateTime = currentMillis; // 最終更新時間を更新
+        }
+    }
+
     // 現在時刻を取得
     time_t now = time(NULL);
     struct tm *now_tm = localtime(&now);
@@ -129,6 +162,9 @@ void update_time(lv_timer_t *timer) {
 
     // 時刻を表示するラベルを更新
     lv_label_set_text(time_label, time_str);
+
+    // TFT表示用に時刻をシリアル出力
+    Serial.println(time_str);
 }
 
 
