c0d936888ef7190d4577ccce6546dc36fd000d16
techhouse_150908 <kmaehara@mel.co.jp>
2024-01-30 10:47:40 +0900

    ポート設定OK

--- src/led.cpp
+++ src/led.cpp
@@ -6,14 +6,16 @@
 #include <freertos/task.h>
 #include <freertos/semphr.h>
 
-Adafruit_MCP23X17 mcp_Secur_LED;
-Adafruit_MCP23X17 mcp_local_input;
-Adafruit_MCP23X17 mcp_emg_LED;
+Adafruit_MCP23X17 mcp[5];
+//Adafruit_MCP23X17 mcp_local_input;
+//Adafruit_MCP23X17 mcp_emg_LED;
 
 SemaphoreHandle_t ledSemaphore; // セマフォを定義
 
 // led.cpp
 bool ledOn = false; // グローバル変数を定義
+
+#define MCP_BASE_ADDR 0x20
 
 // LED点滅タスク
 void blinkLedTask(void *parameter) {
@@ -25,12 +27,12 @@ void blinkLedTask(void *parameter) {
           if (ledOn) {
               // 全てのLEDを点灯
               for (int i = 0; i < 8; i++) {
-                  mcp_Secur_LED.digitalWrite(i, HIGH);
+                  mcp[0x21 - MCP_BASE_ADDR].digitalWrite(i, HIGH);
               }
           } else {
               // 全てのLEDを消灯
               for (int i = 0; i < 8; i++) {
-                  mcp_Secur_LED.digitalWrite(i, LOW);
+                  mcp[0x21 - MCP_BASE_ADDR].digitalWrite(i, LOW);
               }
           }
         }
@@ -38,20 +40,23 @@ void blinkLedTask(void *parameter) {
     }
 }
 
+
+
+
 void led_setup() {
   Wire.begin(26, 25); // ESP32のIO26(SDA)とIO25(SCL)を指定
-  mcp_Secur_LED.begin_I2C(0x21, &Wire); // MCP23017のI2Cアドレスを指定（必要に応じて変更）
+  mcp[0x21 - MCP_BASE_ADDR].begin_I2C(0x21, &Wire); // MCP23017のI2Cアドレスを指定（必要に応じて変更）
   
   // LEDを出力に設定
-  for (int i = 0; i < 8; i++) {
-    mcp_Secur_LED.pinMode(i, OUTPUT);
+  for (int i = 0; i < 16; i++) {
+    mcp[0x21 - MCP_BASE_ADDR].pinMode(i, OUTPUT);
   }
 
-  mcp_local_input.begin_I2C(0x22, &Wire);  // MCP23017のI2Cアドレスを0x22として初期化
+  mcp[0x22 - MCP_BASE_ADDR].begin_I2C(0x22, &Wire);  // MCP23017のI2Cアドレスを0x22として初期化
 
   // GPA0からGPA7までを入力として設定
-  for (int i = 0; i < 8; i++) {
-    mcp_local_input.pinMode(i, INPUT);
+  for (int i = 0; i < 16; i++) {
+    mcp[0x22 - MCP_BASE_ADDR].pinMode(i, INPUT);
   }
 
 
