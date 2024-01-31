70ca3ab078ac9348fa88b4eb1fc8504a4d05dea8
techhouse_150908 <kmaehara@mel.co.jp>
2024-01-31 16:45:07 +0900

    一旦コミット。

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,12 @@
-//#pragma execution_character_set("utf-8")
-
 #include <Arduino.h>
-#include <lvgl.h>
-#include <TFT_eSPI.h> // ILI9488ドライバを含むライブラリ
-#include "common.h"
-#include <WiFi.h> // WiFiライブラリをインクルード
+
 
 void setup() {
-    //start_setup();
-    lvgl_screen_setup();
-    //clock_setup();
-    task_setup();
-    screen_setup();
-    buzzer_setup();
-    sensor_input_setup();
-    led_setup();
 
-    WiFi.begin(ssid, password);
 }
 
 
 
 void loop() {
-    lv_task_handler();
-    delay(5);
+
 }
