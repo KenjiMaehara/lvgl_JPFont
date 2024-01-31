975208df522038976635a90b266af1ab70d6f400
techhouse_150908 <kmaehara@mel.co.jp>
2024-01-31 17:45:21 +0900

    ブザー音だけ消すOK

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,9 @@ void setup() {
   // すべてのデバイスのGPB0～7と、アドレス0x21、0x23、0x24のGPA0～7を出力として設定
   for (int addr = 0; addr < 5; addr++) {
     for (int pin = 8; pin < 16; pin++) {
-      mcp[addr].pinMode(pin, OUTPUT);
+      if (!(addr == 0 && pin == 11)) { // アドレス0x20のGPB3を除外
+        mcp[addr].pinMode(pin, OUTPUT);
+      }
     }
     if (addr != 0 && addr != 2) {  // 0x20と0x22を除く
       for (int pin = 0; pin < 8; pin++) {
@@ -36,7 +38,9 @@ void loop() {
   // すべてのデバイスのすべての出力ピンをHIGHに設定（LED ON）
   for (int addr = 0; addr < 5; addr++) {
     for (int pin = 8; pin < 16; pin++) {
-      mcp[addr].digitalWrite(pin, HIGH);
+      if (!(addr == 0 && pin == 11)) { // アドレス0x20のGPB3を除外
+        mcp[addr].digitalWrite(pin, HIGH);
+      }
     }
     if (addr != 0 && addr != 2) {  // 0x20と0x22を除く
       for (int pin = 0; pin < 8; pin++) {
@@ -49,7 +53,9 @@ void loop() {
   // すべてのデバイスのすべての出力ピンをLOWに設定（LED OFF）
   for (int addr = 0; addr < 5; addr++) {
     for (int pin = 8; pin < 16; pin++) {
-      mcp[addr].digitalWrite(pin, LOW);
+      if (!(addr == 0 && pin == 11)) { // アドレス0x20のGPB3を除外
+        mcp[addr].digitalWrite(pin, LOW);
+      }
     }
     if (addr != 0 && addr != 2) {  // 0x20と0x22を除く
       for (int pin = 0; pin < 8; pin++) {
