f78d0863f5e68b03aa52da4514eed0eb96204de8
techhouse_150908 <kmaehara@mel.co.jp>
2024-02-09 10:38:59 +0900

    全部のタスクを止めてみたところ、再起動動作はなくなったことを確認。

--- src/common.h
+++ src/common.h
@@ -89,12 +89,12 @@ extern Preferences preferences;
 
 
 
-#define ENABLE_WIFI_TASK
-#define ENABLE_RFID_TASK
-#define ENABLE_BUZZER_TASK
-#define ENABLE_LED_TASK
-#define ENABLE_SENSOR_INPUT_TASK
-#define ENABLE_APMODE_TASK
+//#define ENABLE_WIFI_TASK
+//#define ENABLE_RFID_TASK
+//#define ENABLE_BUZZER_TASK
+//#define ENABLE_LED_TASK
+//#define ENABLE_SENSOR_INPUT_TASK
+//#define ENABLE_APMODE_TASK
 
 
 #endif
