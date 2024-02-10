18dd038974a4ca76fcc6096fb56de24842af75c8
KenjiMaehara <kmaehara@mel.co.jp>
2024-02-10 12:36:12 +0900

    さらにプログラムを追加

--- src/card.cpp
+++ src/card.cpp
@@ -2,6 +2,20 @@
 #include <Arduino.h>
 #include "common.h"
 
+
+#define MAX_CARD_LENGTH 38 // 最大カードデータ長を定義
+
+#define		CARD_125KHZ		0xfd
+#define		CARD_FELICA		0xfc
+#define		CARD_14443A		0xfb
+#define		CARD_14443B		0xfa
+#define		CARD_15693		0xf8
+#define		CARD_START		0xfe
+
+size_t read_length;
+uint8_t read_buf[MAX_CARD_LENGTH];
+
+
 HardwareSerial RFIDSerial(1); // UART1を使用
 
 // タスクで使用するためのピン設定
@@ -22,7 +36,38 @@ void readRFIDTask(void *parameter) {
       Serial.println(id);
       Serial.print("Data Length: ");
       Serial.println(dataLength);
+
+      // 受信データがMAX_CARD_LENGTH（38バイト）以上かどうかの判定
+      if(dataLength <= MAX_CARD_LENGTH) {
+        Serial.println("受信データはMAX_CARD_LENGTHバイト以上です。");
+      } else {
+        Serial.println("受信データはMAX_CARD_LENGTHバイト未満です。");
+      }
+
+      RFIDSerial.readBytes(read_buf,read_length);
+
+      if(read_buf[0] == CARD_START
+          && ( ( read_length == 38 && read_buf[38 -1] == CARD_FELICA)
+              || ( read_length == 12 && read_buf[12 -1] == CARD_14443A)
+              || ( read_length == 26 && read_buf[26 -1] == CARD_14443B)
+              || ( read_length == 22 && read_buf[22 -1] == CARD_15693)
+              )    
+        )                    
+      {
+        if( read_length == 38 && read_buf[38 -1] == CARD_FELICA) {
+          Serial.println("FeliCaカード");
+        } else if( read_length == 12 && read_buf[12 -1] == CARD_14443A) {
+          Serial.println("Mifareカード");
+        } else if( read_length == 26 && read_buf[26 -1] == CARD_14443B) {
+          Serial.println("Mifareカード");
+        } else if( read_length == 22 && read_buf[22 -1] == CARD_15693) {
+          Serial.println("ISO15693カード");
+        }
+      } else {
+        Serial.println("不明なカード");
+      }
     }
+
     vTaskDelay(10 / portTICK_PERIOD_MS); // タスクのディレイ
   }
 }
