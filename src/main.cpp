48281cb4964d1fcc55b6685088cd48f46508582d
techhouse_150908 <kmaehara@mel.co.jp>
2024-02-26 17:50:14 +0900

    ヒープメモリの空き容量を5秒毎に計測するプログラムを追加。

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include <SPIFFS.h>
 #include <FS.h>
 #include "AWSPub.h"
+#include "esp_heap_caps.h"
 
 void setup() {
     Serial.begin(115200);
@@ -35,8 +36,25 @@ void setup() {
 }
 
 
+unsigned long previousMillis = 0;  // 最後のメモリログ出力時刻を保持する変数
+const long interval = 5000;        // メモリログ出力の間隔をミリ秒で設定 (5000ミリ秒 = 5秒)
+
 
 void loop() {
     lv_task_handler();
     delay(5);
+
+    // 現在の時刻を取得
+    unsigned long currentMillis = millis();
+
+    // 前回のデバッグ出力から5秒以上経過しているかチェック
+    if (currentMillis - previousMillis >= interval) {
+        // 最後のメモリログ出力時刻を更新
+        previousMillis = currentMillis;
+
+        // 現在のフリーヒープメモリサイズを取得してログ出力
+        Serial.print("Free heap size: ");
+        Serial.println(esp_get_free_heap_size());
+    }
+
 }
