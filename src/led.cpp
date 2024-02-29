b9a04517dc40f23eedbbf27aaa334b0457ceb4c8
techhouse_150908 <kmaehara@mel.co.jp>
2024-02-29 17:25:57 +0900

    一部タスクのスタックサイズを変更

--- src/led.cpp
+++ src/led.cpp
@@ -68,7 +68,7 @@ void led_setup() {
   
   #ifdef ENABLE_LED_TASK
   // LED点滅タスクを作成
-  xTaskCreate(blinkLedTask, "Blink LED Task", 2048, NULL, 1, NULL);
+  xTaskCreate(blinkLedTask, "Blink LED Task", 1024 * 4, NULL, 1, NULL);
   #endif
 }
 
