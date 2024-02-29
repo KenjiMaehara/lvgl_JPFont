a811c95f5bbed52587399a7c950134fc04da8cf7
techhouse_150908 <kmaehara@mel.co.jp>
2024-02-29 18:08:43 +0900

    動的割り当てから、静的割り当てに変更

--- src/led.cpp
+++ src/led.cpp
@@ -10,50 +10,35 @@
 
 
 SemaphoreHandle_t ledSemaphore; // セマフォを定義
-
-
-#define EMG_INPUT_CH1 0
-#define EMG_INPUT_CH2 1
-#define EMG_INPUT_CH3 2
-#define EMG_INPUT_CH4 3
-#define EMG_INPUT_CH5 4
-#define EMG_INPUT_CH6 5
-#define EMG_INPUT_CH7 6
-#define EMG_INPUT_CH8 7
-
-
 LedState gLedState;
+
+// MCP23017デバイスの静的インスタンス
+Adafruit_MCP23X17 mcp_0x21;
 
 // LED点滅タスク
 void blinkLedTask(void *parameter) {
-    Serial.println("--------------blinkLedTask Start--------------");
+
+  Serial.println("--------------blinkLedTask Start--------------");
+
+  // 静的に割り当てられたインスタンスの初期化
+  mcp_0x21.begin_I2C(0x21, &Wire);
+  for (int i = 0; i < 16; i++) {
+      mcp_0x21.pinMode(i, OUTPUT);
+  }
 
   //ledState.areaLeds[0] = false;
-
-    while (true) {
-        if (xSemaphoreTake(ledSemaphore, portMAX_DELAY)) {
-          // MCP23017デバイスのインスタンスを動的に作成
-          Adafruit_MCP23X17* mcp_0x21 = new Adafruit_MCP23X17();
-
-          mcp_0x21->begin_I2C(0x21, &Wire);
-          for (int i = 0; i < 16; i++) {
-            mcp_0x21->pinMode(i, OUTPUT);
-          }
-          // セマフォを取得できたらAREA_LEDを状態に応じて点灯/消灯
-          for (int i = 0; i < 8; i++) {
-              mcp_0x21->digitalWrite(i, gLedState.areaLeds[i]);
-          }
-
-          // 入力に応じてEMG_LEDを点灯/消灯
-          for (int i = 0; i < 8; i++) {
-            mcp_0x21->digitalWrite(i+8, gLedState.emgLeds[i]);
-          }
-
-          // MCP23017デバイスのインスタンスを解放
-          delete mcp_0x21;
-        }
-
+  while (true) {
+      if (xSemaphoreTake(ledSemaphore, portMAX_DELAY)) {
+      // セマフォを取得できたらAREA_LEDを状態に応じて点灯/消灯
+      for (int i = 0; i < 8; i++) {
+           mcp_0x21.digitalWrite(i, gLedState.areaLeds[i]);
+      }
+      // 入力に応じてEMG_LEDを点灯/消灯
+       for (int i = 0; i < 8; i++) {
+           mcp_0x21.digitalWrite(i+8, gLedState.emgLeds[i]);
+      }
     }
+  }
 }
 
 
