ad3e9b6e7a2c99a1ae475a3d1ce00ed14c04fb19
KenjiMaehara <kmaehara@mel.co.jp>
2024-03-02 21:23:51 +0900

    一旦コミット。

--- src/screens/security_screen.cpp
+++ src/screens/security_screen.cpp
@@ -12,7 +12,7 @@ static void event_handler(lv_event_t * e);
 
 lv_obj_t* time_label_security; // セキュリティスクリーン用の時刻表示ラベル
 
-void create_security_screen(lv_obj_t *scr) {
+void create_security_screen(void) {
 
     delete_current_lvgl_screen(); // 前の画面を削除
 
@@ -20,11 +20,14 @@ void create_security_screen(lv_obj_t *scr) {
 
     lv_obj_t* screen = lv_obj_create(NULL);  // スクリーンを作成
 
+    #if 1
     // 時刻表示用のラベルを作成
     time_label_security = lv_label_create(screen);
     lv_obj_align(time_label_security, LV_ALIGN_TOP_MID, 0, 5); // 画面の上中央に配置
     lv_label_set_text(time_label_security, "00:00"); // 初期テキスト
 
+
+    
     // ボタンの作成と設定
     lv_obj_t * btn_02 = lv_btn_create(screen); // ボタンを作成
     lv_obj_set_size(btn_02, 400, 100); // ボタンのサイズ設定
@@ -39,10 +42,10 @@ void create_security_screen(lv_obj_t *scr) {
     // 初期スタイル設定
     lv_obj_set_style_bg_color(btn_02, lv_color_make(0, 0, 0), 0); // 黒色に設定
     lv_obj_set_style_text_color(label_02, lv_color_make(255, 255, 255), 0); // テキストを白色に設定
-
+    #endif
 
     //add_navigation_buttons(scr, screen1, screen2);
-
+    load_screen(screen);    // 画面を表示
     Serial.println("create_security_screen End");
 }
 
