aa0fbb78ced359fd8327c622d5330a8a394cb078
KenjiMaehara <kmaehara@mel.co.jp>
2024-03-09 12:08:16 +0900

    3画面目も改造完了

--- src/screens/security_screen.cpp
+++ src/screens/security_screen.cpp
@@ -9,43 +9,51 @@
 
 //static void btn_event_cb(lv_event_t *e);
 static void event_handler(lv_event_t * e);
+static void btn_event_handler3(lv_event_t* e);
 
 lv_obj_t* time_label_security; // セキュリティスクリーン用の時刻表示ラベル
+static lv_obj_t* screen3 = nullptr;
 
 void create_security_screen(void) {
 
-    delete_current_lvgl_screen(); // 前の画面を削除
-
     Serial.println("create_security_screen Start");
+    if (screen3 == nullptr) {
 
-    gScreen = lv_obj_create(NULL);  // スクリーンを作成
+        screen3 = lv_obj_create(NULL);  // スクリーンを作成
 
-    #if 0
-    // 時刻表示用のラベルを作成
-    time_label_security = lv_label_create(gScreen);
-    lv_obj_align(time_label_security, LV_ALIGN_TOP_MID, 0, 5); // 画面の上中央に配置
-    lv_label_set_text(time_label_security, "00:00"); // 初期テキスト
+        #if 1
+        // 時刻表示用のラベルを作成
+        time_label_security = lv_label_create(screen3);
+        lv_obj_align(time_label_security, LV_ALIGN_TOP_MID, 0, 5); // 画面の上中央に配置
+        lv_label_set_text(time_label_security, "00:00"); // 初期テキスト
 
 
-    
-    // ボタンの作成と設定
-    lv_obj_t * btn_02 = lv_btn_create(gScreen); // ボタンを作成
-    lv_obj_set_size(btn_02, 400, 100); // ボタンのサイズ設定
-    lv_obj_center(btn_02); // ボタンを中心に配置
-    lv_obj_add_event_cb(btn_02, event_handler, LV_EVENT_ALL, NULL);
 
-    // ボタンのラベルを設定
-    lv_obj_t * label_02 = lv_label_create(btn_02);
-    lv_label_set_text(label_02, "Disarmed");
-    lv_obj_center(label_02);
+        // ボタンの作成と設定
+        lv_obj_t * btn_02 = lv_btn_create(screen3); // ボタンを作成
+        lv_obj_set_size(btn_02, 400, 100); // ボタンのサイズ設定
+        lv_obj_center(btn_02); // ボタンを中心に配置
+        lv_obj_add_event_cb(btn_02, event_handler, LV_EVENT_ALL, NULL);
 
-    // 初期スタイル設定
-    lv_obj_set_style_bg_color(btn_02, lv_color_make(0, 0, 0), 0); // 黒色に設定
-    lv_obj_set_style_text_color(label_02, lv_color_make(255, 255, 255), 0); // テキストを白色に設定
-    #endif
+        // ボタンのラベルを設定
+        lv_obj_t * label_02 = lv_label_create(btn_02);
+        lv_label_set_text(label_02, "Disarmed");
+        lv_obj_center(label_02);
 
-    //add_navigation_buttons(scr, screen1, screen2);
-    load_screen(gScreen);    // 画面を表示
+        // 初期スタイル設定
+        lv_obj_set_style_bg_color(btn_02, lv_color_make(0, 0, 0), 0); // 黒色に設定
+        lv_obj_set_style_text_color(label_02, lv_color_make(255, 255, 255), 0); // テキストを白色に設定
+        
+        lv_obj_t* btn3 = lv_btn_create(screen3);
+        lv_obj_add_event_cb(btn3, btn_event_handler3, LV_EVENT_CLICKED, NULL);
+        lv_obj_align(btn3, LV_ALIGN_TOP_LEFT, 10, 10); // ボタンの位置を画面の左上に設定
+        lv_obj_t* label3 = lv_label_create(btn3);
+        lv_label_set_text(label3, "Go to Screen 4");
+        
+        #endif
+    }
+    lv_scr_load(screen3);
+
     Serial.println("create_security_screen End");
 }
 
@@ -81,3 +89,7 @@ static void event_handler(lv_event_t * e) {
 }
 
 
+static void btn_event_handler3(lv_event_t* e) {
+    //show_screen4(); // Screen 4へ遷移
+    create_clock_screen();
+}
